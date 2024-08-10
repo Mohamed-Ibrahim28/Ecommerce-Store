399c3905ef0bc7271aa59058f21403594a16482b
Mohamed Ibrahim <medoelprinse28@gmail.com>
2024-08-10 16:07:19 +0300

    edit style navebar

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -7,19 +7,24 @@ import {
   Badge,
   IconButton,
   Container,
+  Box,
 } from "@mui/material";
-// icons
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// context
 import { CardContext } from "../../contexts/CardContextProvider";
+
+// Import your logo image
+import logo from "../../assets/img/retail-shop.png";
 
 const Navbar = () => {
   const { state } = useContext(CardContext);
+
   return (
     <AppBar
       position="static"
+      elevation={1}
       sx={{
-        backgroundColor: "#b7b7b7",
+        backgroundColor: "#282c33", // Solid background color for a modern look
+        color: "#ffffff", // White text color
       }}
     >
       <Container maxWidth="lg">
@@ -27,17 +32,42 @@ const Navbar = () => {
           sx={{
             display: "flex",
             justifyContent: "space-between",
+            alignItems: "center",
+            padding: "0.75rem 1rem", // Adequate padding for spacing
           }}
         >
-          <Link to="/">
-            <Typography variant="h6" component="div" color="text.secondary">
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <img
+                src={logo}
+                alt="Store Logo"
+                style={{ height: "40px", marginRight: "1rem" }}
+              />
+            </Link>
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "#ffffff",
+                fontSize: "1.2rem",
+                fontWeight: "bold",
+              }}
+            >
               Store
-            </Typography>
-          </Link>
-          <Link to="/cards">
-            <IconButton>
-              <Badge badgeContent={state.itemsCounter} color="info">
-                <ShoppingCartIcon color="action" sx={{ fontSize: "30px" }} />
+            </Link>
+          </Box>
+          <Link to="/cards" style={{ color: "inherit" }}>
+            <IconButton color="inherit">
+              <Badge badgeContent={state.itemsCounter} color="secondary">
+                <ShoppingCartIcon
+                  sx={{
+                    fontSize: "30px",
+                    transition: "transform 0.3s",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
+                  }}
+                />
               </Badge>
             </IconButton>
           </Link>
